c1362dbf42e764268f0b522b6504cb77fa95d3ca
Phil Ma <yuhao93@gmail.com>
2016-12-05 22:39:03 -0800

    Fixed build.sh merge conflict

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 rm -rf out && mkdir -p out && \
 rm -rf gen && mkdir -p gen && \
 protoc -I=proto --cpp_out=gen proto/*.proto && \
-<<<<<<< HEAD
-g++ -o out/sisyphus -std=c++0x src/*.cc gen/*.cc -Ilinux/include -Iinclude -Igen -lwiringPi -lpthread -lprotobuf
-=======
-g++ -o out/sisyphus -std=c++0x src/*.cc gen/*.cc linux/src/*.cc -Iinclude -Igen -lwiringPi -lpthread -lprotobuf
->>>>>>> 7810e18c2708e4d4eb454b3db4c579eb96af0773
+g++ -o out/sisyphus -std=c++0x src/*.cc gen/*.cc linux/src/*.cc -Ilinux/include -Iinclude -Igen -lwiringPi -lpthread -lprotobuf
