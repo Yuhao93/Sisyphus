a6b64a61c50f82f8b387446958d9d7ec20c92c81
Phil Ma <yuhao93@gmail.com>
2020-04-11 02:37:07 +0000

    Added table contants

--- include/table_constants.h
+++ include/table_constants.h
@@ -2,6 +2,10 @@
 #define TABLE_CONSTANTS_H
 
 #define ANGULAR_STEPS_PER_REVOLUTION (200 * 8 * 32)
-#define LINEAR_STEPS_PER_SWEEP 48693
+#define LINEAR_STEPS_PER_SWEEP 42400
+
+
+#define zero 270
+#define max 40450
 
 #endif
