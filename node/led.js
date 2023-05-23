7b9b8402fee393f521649fb4cd43a52463deb413
Phil Ma <yuhao93@gmail.com>
2023-05-22 23:45:25 -0700

    A

--- node/led.js
+++ node/led.js
@@ -4,7 +4,7 @@ let v;
 
 function setIntensity(intensity) {
   v = Math.max(Math.min(1, intensity), 0);
-  const valueInRange = Math.floor(gpio.GPIO_LED_RANGE * v);
+  const valueInRange = Math.floor(gpio.GPIO_LED_RANGE * (1 - v));
   gpio.pwmWrite(gpio.GPIO_LED, valueInRange);
 }
 
